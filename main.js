298748dd3247c666be6d8097d8e637ea65be9c52
lucasGeek2020 <62662983+lucasGeek2020@users.noreply.github.com>
2020-04-24 23:12:05 -0300

    JS

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 
-var binario = document.querySelector("#binario");
-converter(binario);
 
-function converter(bin){
-    
+//variaveis dos inputs
+var binario = document.querySelector("#binario");
+var decimal = document.querySelector("#decimal")
+
+
+//funcao quando musar o valor input
+binario.oninput = function(){
+
+    document.addEventListener('keyup', logKey);
+    function logKey(e) {
+        console.log(e.code)
+       if(e.code !== "Digit1" && e.code !== "Numpad0" && e.code !== "Digit0" && e.code !== "Numpad1"){
+            console.log("náo é binario");
+        }
+      }
+    //validar binario
+    /*
+    if(binario.value == 1){
+        console.log("náo é binario")
+    }
+    */
 }
+
+
+
 
